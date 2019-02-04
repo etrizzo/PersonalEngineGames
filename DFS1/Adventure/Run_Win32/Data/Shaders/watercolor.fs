bf86c157d42078c0c5494f37b87c9ea48f093ff6
Emily Rizzo <emilytrizzo@gmail.com>
2019-02-04 10:39:47 -0600

    added uniforms to tweak in watercolor shader

--- DFS1/Adventure/Run_Win32/Data/Shaders/watercolor.fs
+++ DFS1/Adventure/Run_Win32/Data/Shaders/watercolor.fs
@@ -5,6 +5,21 @@
 layout(binding = 0) uniform sampler2D gTexDiffuse;
 layout(binding = 4) uniform sampler2D gTexDisplacement;
 
+uniform float WATERCOLOR_TIME_DILATION = 1.5;
+
+uniform float EDGE_GRADIENT_RANGE = 1.5;
+uniform float EDGE_CRISPNESS = .8;
+uniform float OUTLINE_STRENGTH = 10.0;
+uniform float EDGE_GRADIENT_STRENGTH = .7;    //1-this gets added to the random to brighten the edges from noise
+uniform float EDGE_SCROLL_TIME_SCALE = .15;
+uniform float GRAYSCALE_WEIGHT = .95;  //good at .75 for full map
+uniform float HALFTONE_WEIGHT = .55;
+
+uniform float WATERCOLOR_WEIGHT = 1.45;
+uniform float EDGE_WEIGHT = 2.5;
+uniform float SATURATION = .15;
+
+uniform float EDGE_MIX_WEIGHT = .85;
 
 
 in vec4 passColor;
@@ -51,11 +66,11 @@ vec4 GetColor(vec2 uvs)
    return texture(gTexDiffuse, uvs);
 }
 
-vec4 GetRandomWithTimeDisplacement(vec2 uvs)
+vec4 GetRandomWithDisplacement(vec2 uvs)
 {
    //return texture(gTexDisplacement, uvs);
   return texture(gTexDisplacement, uvs + vec2(-TEXEL_SIZE_X, TEXEL_SIZE_Y * .5) * 70.0 );
-   return texture(gTexDisplacement, uvs + (  TEXEL_SIZE_X * 150.0 * sin(GAME_TIME)));
+   //return texture(gTexDisplacement, uvs + (  TEXEL_SIZE_X * 150.0 * sin(GAME_TIME)));
 }
 
 vec4 GetRandom(vec2 uvs)
@@ -83,9 +98,10 @@ vec2 GetGradient(vec2 uvs, float delta)
 float htPattern(vec2 pos)
 {
     float p;
-    vec2 timeDisplacement = vec2(-TEXEL_SIZE_X, TEXEL_SIZE_Y * .5) * 100.0 * GAME_TIME * .15;
-    float r=GetRandom(pos*.1 + vec2(.0255, .0265) + timeDisplacement).x;     //down-res the noise, [0,1] value
-    float rAdjusted = (r +.3) * (GetRandom(pos + timeDisplacement ).r + .4) ;         //combine the low resolution black/white noise with regular noise to make it crunchier
+    vec2 timeDisplacement = vec2(-TEXEL_SIZE_X, TEXEL_SIZE_Y * .5) * 100.0 * GAME_TIME * EDGE_SCROLL_TIME_SCALE;
+    float r=GetRandom(pos*.3  + timeDisplacement).x;     //down-res the noise, [0,1] value
+    //combine the low resolution black/white noise with regular noise to get a perlin effect
+    float rAdjusted = (r +(1.0 - EDGE_GRADIENT_STRENGTH)) * (GetRandom(pos + timeDisplacement ).r + .4) ;         
     p=clamp(rAdjusted ,0.0,1.0); //square value of noise to get starker difference, then subtract to get more darks
     return p * .9;
 }
@@ -103,7 +119,7 @@ vec4 getBWDist(vec2 pos)
     //smooth step from .9 to 1.1 wherethe interpolation is a weight between the gray value and a down-res of the noise at that point.
     //so light/dark areas will be less noisy while neutral areas will have a noise pattern.
     //the weight of the grayscale is the actual edges, the weight of htPattern is the full screen noisyness
-    return vec4(smoothstep(.9,1.1,GetGrayscale(pos)*.95+htPattern(pos*.4)*.55));
+    return vec4(smoothstep(.9,1.1,GetGrayscale(pos)*GRAYSCALE_WEIGHT + htPattern(pos*.4)*HALFTONE_WEIGHT));
 }
 
 
@@ -149,11 +165,11 @@ void main()
         //get some random offsets?
         //get a random vec2, range map to [-.5 , .5]
         //then scale it down to be .0001x size        vv rangemapping to [-.5,5]
-        vec2 randomSmallOffset1 = randomOffsetScale * ((GetRandomWithTimeDisplacement(positiveEdgePosition).xy - vec2(.5)) * texelSize);
-        vec2 randomSmallOffset2 = randomOffsetScale * ((GetRandomWithTimeDisplacement(negativeEdgePosition).xy - vec2(.5)) * texelSize);
+        vec2 randomSmallOffset1 = randomOffsetScale * ((GetRandomWithDisplacement(positiveEdgePosition).xy - vec2(.5)) * texelSize);
+        vec2 randomSmallOffset2 = randomOffsetScale * ((GetRandomWithDisplacement(negativeEdgePosition).xy - vec2(.5)) * texelSize);
         
-        vec2 randomSmallOffset3 = randomOffsetScale * ((GetRandomWithTimeDisplacement(positiveWatercolorPosition).xy - vec2(.5)) * texelSize);
-        vec2 randomSmallOffset4 = randomOffsetScale * ((GetRandomWithTimeDisplacement(negativeWatercolorPosition).xy - vec2(.5)) * texelSize);
+        vec2 randomSmallOffset3 = randomOffsetScale * ((GetRandomWithDisplacement(positiveWatercolorPosition).xy - vec2(.5)) * texelSize);
+        vec2 randomSmallOffset4 = randomOffsetScale * ((GetRandomWithDisplacement(negativeWatercolorPosition).xy - vec2(.5)) * texelSize);
         
         //==============================
         //calculate for watercolor effect
@@ -167,29 +183,29 @@ void main()
         float distanceFactor = 1.0 - (float(i) /(float(sampleNum)* 1.0));        //closer samples are more effective?
         
         
-        float gradiantScale3 = 1.5 * ((sin(GAME_TIME + 2) + 2.0f) * .25);    //started at .25
+        float gradiantScale3 = 1.5 * ((sin(GAME_TIME * WATERCOLOR_TIME_DILATION + 2) + 2.0f) * .25);    //started at .25
         float gradiantScale4 = .75;    //started at .5
         
         //scales down the range of where we sample our noise
         //so that we get a chunkier texture?
         //bc we're sampling from a lower resolution area?
-        float noiseSampleScale = .05;     //started at .07
+        float noiseSampleScale = .1;     //started at .07
         
         // colors + wash effect on gradients:
         // color gets lost from dark areas to bright areas
         //without the normalize, the image gets much crisper
         //bc the values are much smaller? so normalizing makes the displacement larger
         //moving the positions creates like a smudging effect because the color accumulates along the gradiant
-        vec2 positiveUVChange = (gradiantScale3*normalize(gr3)) + .5*(GetRandomWithTimeDisplacement(passUV*noiseSampleScale).xy-.7);
-        vec2 negativeUVChange = (gradiantScale4*normalize(gr4)) + .5*(GetRandomWithTimeDisplacement(passUV*noiseSampleScale).xy-.7);
+        vec2 positiveUVChange = (gradiantScale3*normalize(gr3)) + .5*(GetRandomWithDisplacement(passUV*noiseSampleScale).xy-.7);
+        vec2 negativeUVChange = (gradiantScale4*normalize(gr4)) + .5*(GetRandomWithDisplacement(passUV*noiseSampleScale).xy-.7);
         positiveWatercolorPosition+= positiveUVChange * vec2(TEXEL_SIZE_X, TEXEL_SIZE_Y);     //dark areas - positive along gradiant
         negativeWatercolorPosition-= positiveUVChange * vec2(TEXEL_SIZE_X, TEXEL_SIZE_Y);     //bright areas - negative along gradiant
         
         float f1=6.0 * distanceFactor;     //amplify close to me
         float f2=4.0 *(.7-distanceFactor);    //amplify things within .7 of me??
-        //how close you are * (25% your color + 40%noiseColor));
-        watercolor3+=f1*(GetColor(positiveWatercolorPosition).xyz +vec3(.25)+ .05*(GetRandomWithTimeDisplacement(positiveEdgePosition*1.0).xyz - .5));
-        watercolor3+=f2*(GetColor(negativeWatercolorPosition).xyz +vec3(.25)+ .05*(GetRandomWithTimeDisplacement(negativeWatercolorPosition*1.0).xyz - .5));
+        //how close you are * (your color + 25% + 5% noiseColor));
+        watercolor3+= f1*(GetColor(positiveWatercolorPosition).xyz +vec3(.25)+ .05*(GetRandomWithDisplacement(positiveEdgePosition*1.0).xyz - .5));
+        watercolor3+= f2*(GetColor(negativeWatercolorPosition).xyz +vec3(.25)+ .05*(GetRandomWithDisplacement(negativeWatercolorPosition*1.0).xyz - .5));
         
         count2+=f1+f2;      //base color factor?
         //count+=fact;        //edge factor?
@@ -199,19 +215,19 @@ void main()
         //calculate for edge effect
 
         
-        float edgeDelta = vign * 1.5;
+        float edgeDelta = vign * EDGE_GRADIENT_RANGE;
         vec2 gr =GetGradient(positiveEdgePosition, edgeDelta)+ randomSmallOffset1;
         vec2 gr2=GetGradient(negativeEdgePosition, edgeDelta)+ randomSmallOffset2;
         
         //amplify the length so that the strong ones are 1 and everything else is at least visible
-        float lengthScale = 10.0;     //started at 10.0, lower val means lighter outlines
-        float gr1Strength=clamp(lengthScale*length(gr),0.,1.);
-        float gr2Strength=clamp(lengthScale*length(gr2),0.,1.);
+        float lengthScale = OUTLINE_STRENGTH;     //started at 10.0, lower val means lighter outlines
+        float gr1Strength=clamp(lengthScale*length(gr) , 0.0, 1.0);
+        float gr2Strength=clamp(lengthScale*length(gr2), 0.0, 1.0);
 
         // outlines:
         // stroke perpendicular to gradient
-        positiveEdgePosition +=.8 *normalize(GetPerpendicular(gr)) * texelSize;
-        negativeEdgePosition -=.8 *normalize(GetPerpendicular(gr2))* texelSize;
+        positiveEdgePosition +=EDGE_CRISPNESS *normalize(GetPerpendicular(gr)) * texelSize;
+        negativeEdgePosition -=EDGE_CRISPNESS *normalize(GetPerpendicular(gr2))* texelSize;
 
         edgeColor3+=distanceFactor*mix(vec3(1.2),getBWDist(positiveEdgePosition).xyz*2.,gr1Strength); //use the strength of the gradient to determine
         edgeColor3+=distanceFactor*mix(vec3(1.2),getBWDist(negativeEdgePosition).xyz*2.,gr2Strength);    //if this is an edge or nah ( too low will be mostly wite)
@@ -219,18 +235,19 @@ void main()
         count+=distanceFactor;      //edge factor?
         
    }
-   edgeColor3/= count  * 2.5;
-   watercolor3/= (count2 * 1.45);  //weight of the blobby color
+   edgeColor3/= count  * EDGE_WEIGHT;
+   watercolor3/= (count2 * WATERCOLOR_WEIGHT);  //weight of the blobby color
 
    vec4 waterColor = vec4(watercolor3, 1.0);
-   waterColor = mix(waterColor, diffuse,.15);
+   waterColor = mix(waterColor, diffuse, SATURATION);
    vec4 edgeColor = vec4(edgeColor3, 1.0);
   
 
    //float r = length((passUV-texelSize*.5)/TEXEL_SIZE_X);
    //float vign = (1.0-r) * 2.0;
    //float vign = 0.5 + 0.5*16.0*passUV.x*passUV.y*(1.0-passUV.x)*(1.0-passUV.y);
-   vec4 finalColor = clamp(clamp(edgeColor*.85+.15,0.,1.)*waterColor,0.,1.);
+   vec4 finalColor = clamp(clamp(edgeColor * EDGE_MIX_WEIGHT + (1.0 - EDGE_MIX_WEIGHT), 0.0, 1.0) * waterColor, 0.0, 1.0);
+   //vec4 finalColor = clamp(clamp(edgeColor*.85+.15,0.,1.)*waterColor,0.,1.);
    outColor = (finalColor * passColor) ;
    //outColor = mix(outColor, diffuse, .1)
    //outColor = getBWDist(passUV);
@@ -238,7 +255,7 @@ void main()
    //outColor = waterColor;
    outColor.a = 1.0;
    //vec4 gradiantColor = vec4(normalize(GetGradient(passUV, 1.0)), 0.0, 1.0);
-   //vec2 randomSmallOffset = .0001 * ((GetRandomWithTimeDisplacement(positiveEdgePosition).xy - vec2(.5)) * texelSize);
+   //vec2 randomSmallOffset = .0001 * ((GetRandomWithDisplacement(positiveEdgePosition).xy - vec2(.5)) * texelSize);
    //vec2 gr =GetGradient(positiveEdgePosition, .5)+ randomSmallOffset;
     
 
