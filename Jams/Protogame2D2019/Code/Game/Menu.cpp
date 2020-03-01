a34e9527e0c2d8a11ae4e50f531b93198c1551e4
etrizzo <emilytrizzo@gmail.com>
2020-03-01 11:20:12 -0800

    Adding proto2D for VS2019

---
+++ Jams/Protogame2D2019/Code/Game/Menu.cpp
@@ -0,0 +1,106 @@
+#include "Game/Menu.hpp"
+
+MenuOption::MenuOption(std::string optText, int optValue, bool isActive)
+{
+	m_text = optText;
+	m_value = optValue;
+	m_isActive = isActive;
+
+}
+
+void Menu::HandleInput()
+{
+	if (AcceptJustPressed()){
+		m_selectedOption = true;
+		
+	}
+	if (DownJustPressed()){
+		MoveSelection(1);
+	}
+	if (UpJustPressed()){
+		MoveSelection(-1);
+	}
+
+	
+}
+
+void Menu::Render()
+{
+	float textHeight = m_menuBox.GetHeight() / m_options.size();
+	AABB2 bgBox = m_menuBox;
+	float padding = m_menuBox.GetWidth() * .05f;
+	bgBox.AddPaddingToSides(padding, padding);
+	g_theRenderer->DrawAABB2(bgBox, RGBA(0,64,64,128));
+	g_theRenderer->DrawAABB2Outline(bgBox, RGBA::WHITE);
+
+	//render options
+	AABB2 textBox = AABB2(m_menuBox.mins.x, m_menuBox.maxs.y - textHeight, m_menuBox.maxs.x, m_menuBox.maxs.y);
+	for (int i = 0; i < (int) m_options.size(); i++){
+		MenuOption option = m_options[i];
+		if (i == m_currentIndex){
+			g_theRenderer->DrawTextInBox2D(option.m_text, textBox, Vector2::HALF, textHeight, TEXT_DRAW_SHRINK_TO_FIT, RGBA::GREEN);
+		} else if (!option.m_isActive){
+			g_theRenderer->DrawTextInBox2D(option.m_text, textBox, Vector2::HALF, textHeight, TEXT_DRAW_SHRINK_TO_FIT, RGBA::WHITE.GetColorWithAlpha(128));
+		} else {
+			g_theRenderer->DrawTextInBox2D(option.m_text, textBox, Vector2::HALF, textHeight, TEXT_DRAW_SHRINK_TO_FIT, RGBA::WHITE);
+		}
+		textBox.Translate(0.f, -textHeight);
+	}
+
+
+}
+
+void Menu::AddOption(char const * text, int value, bool isActive)
+{
+	m_options.push_back(MenuOption(text,value,isActive));
+}
+
+void Menu::DisableOption(int value)
+{
+	for (int i = 0; i < (int)m_options.size() ; i++){
+		if (m_options[i].m_value == value){
+			m_options[i].m_isActive = false;
+			break;
+		}
+	}
+	if (!m_options[m_currentIndex].m_isActive){
+		MoveSelection(1);
+	}
+}
+
+void Menu::EnableOption(int value)
+{
+	for (int i = 0; i < (int)m_options.size() ; i++){
+		if (m_options[i].m_value == value){
+			m_options[i].m_isActive = true;
+			break;
+		}
+	}
+}
+
+int Menu::GetFrameSelection()
+{
+	if (m_selectedOption){
+		m_selectedOption = false;
+		return m_options[m_currentIndex].m_value;
+	}
+	return -1;
+}
+
+void Menu::MoveSelection(int direction)
+{
+	int numOptions = m_options.size();
+	int newPos = m_currentIndex + direction;
+	if (newPos < 0){
+		newPos = numOptions - 1;
+	}
+	newPos = newPos % numOptions;
+	while (!m_options[newPos].m_isActive){
+		newPos = newPos + direction;
+		if (newPos < 0){
+			newPos = numOptions - 1;
+		}
+		newPos = newPos % numOptions;
+	}
+	m_currentIndex = newPos;
+}
---
+++ Jams/Protogame2D2019/Run_Win32/Data/Shaders/Includes/lighting.glsl
@@ -0,0 +1,130 @@
+const int MAX_LIGHTS = 8;
+
+//LIGHTING ============================================
+ 
+
+// Scene related
+uniform vec4 AMBIENT; // xyz color, w intensity
+
+
+
+// surface 
+uniform float SPECULAR_AMOUNT; // shininess (0 to 1)
+uniform float SPECULAR_POWER; // smoothness (1 to whatever)
+
+
+struct light_t 
+{
+   vec4 LIGHT_COLOR;
+
+   vec3 LIGHT_POSITION;
+   float padding00;
+   
+   vec3 LIGHT_FORWARD_DIR;
+   float IS_POINT_LIGHT;
+   
+   vec3 LIGHT_ATTENUATION;
+   float DOT_INNER_ANGLE;
+   
+   vec3 SPECULAR_ATTENUATION;
+   float DOT_OUTER_ANGLE;
+}; 
+
+
+struct light_factor_t 
+{
+   vec3 diffuse; 
+   vec3 specular; 
+}; 
+
+layout(binding=2, std140) uniform uboLightBuffer 
+{
+   light_t LIGHTS[MAX_LIGHTS];
+};
+
+
+float GetAttenuation( float i, float d, vec3 a )
+{
+   return i / (a.x + d * a.y + d * d * a.z);
+   //return i / (d * d);
+}
+
+
+//--------------------------------------------------------------------------------------
+light_factor_t CalculateLightFactor( vec3 position, 
+   vec3 eye_dir, 
+   vec3 normal, 
+   light_t light, 
+   float spec_factor, 
+   float spec_power )
+{
+   light_factor_t lf; 
+
+   vec3 light_color = light.LIGHT_COLOR.xyz;
+
+   // get my direction to the light, and distance
+   vec3 light_dir = light.LIGHT_POSITION - position; // direction TO the light
+   float dist = length(light_dir); 
+   light_dir /= dist; 
+
+   // 
+   vec3 light_forward = normalize(light.LIGHT_FORWARD_DIR); 
+
+   // get the power
+   float light_intensity = light.LIGHT_COLOR.w; 
+
+   // figure out how far away angle-wise I am from the forward of the light (useful for spot lights)
+   float dot_angle = dot( light_forward, -light_dir ); 
+
+   // falloff for spotlights.
+   float angle_attenuation = smoothstep( light.DOT_OUTER_ANGLE, light.DOT_INNER_ANGLE, dot_angle ); 
+   light_intensity = light_intensity * angle_attenuation; 
+
+   // get actual direction light is pointing (spotlights point in their "forward", point lights point everywhere (ie, toward the point))
+   light_dir = mix( -light_forward, light_dir,light.IS_POINT_LIGHT);
+
+   float attenuation = clamp( GetAttenuation( light_intensity, dist, light.LIGHT_ATTENUATION ), 0, 1 ); 
+   float spec_attenuation = clamp( GetAttenuation( light_intensity, dist, light.SPECULAR_ATTENUATION ), 0, 1 );   //further falloff because shiny shit is cool
+
+
+   //===== CALCULATE DIFFUSE FOR THE LIGHT =======
+   // Finally, calculate dot3 lighting
+   float dot3 = dot( light_dir, normal ); 
+   float diffuse_factor = clamp( attenuation * dot3, 0.0f, 1.0f );
+
+   
+   //===== CALCULATE SPECULAR FOR THE LIGHT =======
+   vec3 r = reflect(-light_dir, normal); 
+   float spec_amount = max(dot(r, eye_dir), 0.0f); 
+   float spec_intensity = (spec_attenuation * spec_factor) * pow(spec_amount, spec_power); 
+
+   lf.diffuse = light_color * diffuse_factor;
+   lf.specular = light_color * spec_intensity; 
+
+   return lf; 
+}
+
+
+
+light_factor_t CalculateLighting( vec3 world_pos, 
+   vec3 eye_dir, 
+   vec3 normal, 
+   float spec_factor, 
+   float spec_power ) 
+{
+   light_factor_t lf; 
+   lf.diffuse = AMBIENT.xyz * AMBIENT.w; 
+   lf.specular = vec3(0.0f); 
+
+   spec_factor *= spec_factor; 
+   spec_power *= spec_power; 
+
+   for (uint i = 0; i < MAX_LIGHTS; ++i) {
+      light_factor_t l = CalculateLightFactor( world_pos, eye_dir, normal, LIGHTS[i], spec_factor, spec_power ); 
+      lf.diffuse += l.diffuse;
+      lf.specular += l.specular; 
+   }
+
+   lf.diffuse = clamp( lf.diffuse, vec3(0.0f), vec3(1.0f) ); 
+   return lf; 
+}
